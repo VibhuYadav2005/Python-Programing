ba46050890f777e7bca3a171d9c0938fa020ff43
Vibhu <144217754+VibhuYadav2005@users.noreply.github.com>
2023-09-19 17:36:34 +0530

    Add files via upload

---
+++ While Loop Ques and Programs/countdown timer program.py
@@ -0,0 +1,8 @@
+""" Countdown timer :
+write a python program that takea a integer input from the user and counts down from that number to one using whike loop
+"""
+
+n= int(input("Set timer :"))
+while(n>=1):
+    print(n)
+    n-=1
