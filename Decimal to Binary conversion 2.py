1d04dda6dff38de8dafb6ff51f2f09cd2a2aa529
Vibhu <144217754+VibhuYadav2005@users.noreply.github.com>
2023-09-19 17:33:12 +0530

    Delete Decimal to Binary conversion 2.py

--- Decimal to Binary conversion 2.py
+++
@@ -1,9 +0,0 @@
-"""Read an integer number in Decimal and converts it into Binary Number System.
-
-The logic behind to implement this program -
-
-Get remainder using modulus operator by 2 and store it into an array then divide number by 2, repeat this process till given number is greater than 0. Because 2 is the base of Binary Number System."""
-
-n=int(input("ENTER NO. TO CONVERT INTO BINARY:"))
-a=bin(n).replace("0b", "")
-print(a)
